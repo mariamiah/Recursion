df36f272bdfd495f58aa7ad3b36f89434c351462
= <=>
2018-09-21 19:42:46 +0300

    Modify test_power.py file

--- challengeTwo/test_power.py
+++ challengeTwo/test_power.py
@@ -8,7 +8,7 @@ class TestPower(unittest.TestCase):
     
     def test_power_one(self):
         self.assertEqual(power(3, 1), 3)
-        self.assertEqual(power(0, 1),0)
+        self.assertEqual(power(0, 1), 0)
         self.assertEqual(power(-2, 1), -2)
     
     def test_value_type(self):
